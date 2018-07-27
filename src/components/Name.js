9f6df99a7f9009d74cabae2454f6ea0aae7ab922
Marek Borovsky <borovsky@oksystem.cz>
2018-07-27 09:43:57 +0200

    Names font size

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Name = ({name}) =>
-    <h1 className="ui inverted header">
+    <h1 className={`ui inverted header ${name && name.length > 20 ? name.length > 100 ? 'tiny' : 'smaller' : ''}`}>
         {name}
     </h1>;
 
